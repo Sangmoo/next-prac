1f63b2fdf5970599e7c0e6dc4ba9dd58c2e04b3a
Sangmoo <first_1st@naver.com>
2022-01-24 23:53:31 +0900

    use Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
+// import { useState } from "react";
+import NavBar from "../components/NavBar";
+
+// npm run dev
 export default function Home() {
+  // const [counter, setCounter] = useState(0);
   return (
     <div>
+      {/* <h1>Hello {counter}</h1> */}
+      {/* <button onClick={() => setCounter((prev) => prev + 1)}>+</button> */}
+      <NavBar />
       <h1>Hello Next</h1>
     </div>
   );
